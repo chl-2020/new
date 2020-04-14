e8763a33f652e0122c91678fbf864fb8741ac1d6
chl-2020 <2250475953@qq.com>
2020-04-14 17:32:22 +0800

    这是第二个项目

---
+++ src/com/qa/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.qa.dao;
+
+public class UserDao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("abc");
+     System.out.println("******");
+     System.out.println("^^^^^^");
+	}
+
+}
